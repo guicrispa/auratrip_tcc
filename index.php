58627aa6ea91f6f2b1b299b3b2bbff46372cb4cf
guicrispa <guicrispim1000@gmail.com>
2023-05-03 22:48:36 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('head.php') ?>
+<body>
+    <header id="main" class="style_header">
+      <div class="container_header">
+            <a href="index.php"><img class="logo_img" src="assets/auratrip-png.png" alt="logo"></a>
+            <ul class="buttons" id="home">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Criar</a></li>
+                <li><a href="#">Roteiros</a></li>
+                <li><a href="#">Entrar</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <section>
+      
+    </section>
+
+<?php include('footer.php') ?>
