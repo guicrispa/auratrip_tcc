fb447f0bd1dd9fccdccc73beaf72396a7fcceda4
guicrispa <guicrispim1000@gmail.com>
2023-05-09 17:58:14 -0400

    template head and footer js

---
+++ assets/template_footer.js
@@ -0,0 +1,4 @@
+let footer = document.getElementById('footer')[0];
+
+footer.insertAdjacentHTML('afterbegin', `</body>
+</html>`)
---
+++ assets/template_head.js
@@ -0,0 +1,31 @@
+// tag do span #head em uma variável
+let head=document.getElementById('head')[0];
+
+// tag
+
+head.insertAdjacentHTML('afterend', `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta property="og:locale" content="pt_BR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://auratrip.com.br/" />
+    <meta property="og:title" content="Auratrip - TCC Fatec Rubens Lara" />
+    <meta property="og:site_name" content="Auratrip" />
+    <meta property="og:description" content="Auratrip - Planejamento de viagens e Informações sobre lugares" />
+
+    <meta property="og:image" content="https://ik.imagekit.io/bistecbr/bistec_metatag.jpg" />
+    <meta property="og:image:type" content="image/jpg" />
+    <meta property="og:image:alt" content="Logo Auratrip" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="400" />
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Auratrip - TCC Fatec Rubens Lara.">
+    <!-- google site verification -->
+    <meta name="google-site-verification" content="z_lH9tH0wbjtMz-3Gxh-qzwu25t_HTu6qWTVMIgMeiQ" />
+    <title>Auratrip</title>
+    <link rel="icon" href="assets/iconpngauratrip.png">
+    <link rel="stylesheet" href="assets/style.css">
+</head>`)
